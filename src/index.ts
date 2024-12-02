950798769f1cf91e93f29d2462c898678e60181b
aglevillayer <levillayer.annegaelle@gmail.com>
2024-12-02 11:27:18 +0100

    init project and start apollo server

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import "reflect-metadata";
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { buildSchema } from "type-graphql";
+import { CountryResolver } from "./resolvers/CountryResolver";
+
+const port = 4000;
+
+async function startServerApollo() {
+  const schema = await buildSchema({
+    resolvers: [CountryResolver],
+  });
+
+  const server = new ApolloServer({
+    schema,
+  });
+
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+  });
+
+  console.log(`🚀  Server ready at: ${url}`);
+}
+
+startServerApollo();
